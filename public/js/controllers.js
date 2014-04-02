da0597c451270ddf61aa858153b1831af8ef05c0
Aleksandar Terentic <aterentic@gmail.com>
2014-04-02 23:52:17 +0200

    Added new board field on user home page.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,3 +5,32 @@ memoro.controller('UsersController', function ($scope, $http) {
     $scope.users = data;
   });
 });
+
+memoro.controller('UserController', function ($scope, $http) {
+
+  var text = "Add new board...";
+
+  var board = document.getElementById("new-board");
+
+  board.innerHTML = text;
+  board.classList.add("empty");
+
+  board.addEventListener("click", function(event) {
+    if (event.target.classList.contains("empty")) {
+      event.target.innerHTML  = "";
+      event.target.classList.remove("empty");
+    }
+    event.target.contentEditable = true;
+    event.target.focus();
+  });
+
+  board.addEventListener("blur", function(event) {
+    if (event.target.innerHTML == "") {
+      event.target.classList.add("empty");
+      event.target.innerHTML = text;
+    } else {
+      alert("TODO: Add board to user.");
+    }
+  });
+
+});

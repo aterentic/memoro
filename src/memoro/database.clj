9f8aabfb34af3e3b212e15ee7233bcff162a040e
Aleksandar Terentic <aterentic@gmail.com>
2014-07-13 12:00:48 +0200

    Added map destructuring to database function definitions.

--- src/memoro/database.clj
+++ src/memoro/database.clj
@@ -26,11 +26,11 @@
 (defn- read-entity [eid]
   (read-string (pr-str (d/touch (d/entity (read-db) eid)))))
 
-(defn- user-id [user]
-  (first (first (d/q '[:find ?user_id :in $ ?code :where [?user_id :user/code ?code]] (read-db) (:code user)))))
+(defn- user-id [{:keys [code]}]
+  (first (first (d/q '[:find ?user_id :in $ ?code :where [?user_id :user/code ?code]] (read-db) code))))
 
-(defn- board-id [board]
-  (first (first (d/q '[:find ?board_id :in $ ?user ?name :where [?u :user/code ?user] [?u :user/boards ?board_id] [?board_id :board/name ?name]] (read-db) (:user board) (:name board)))))
+(defn- board-id [{:keys [user name]}]
+  (first (first (d/q '[:find ?board_id :in $ ?user ?name :where [?u :user/code ?user] [?u :user/boards ?board_id] [?board_id :board/name ?name]] (read-db) user name))))
 
 (defn make-db []
   (d/create-database uri)
@@ -39,8 +39,8 @@
 (defn delete-db []
   (d/delete-database uri))
 
-(defn add-user [user]
-  (d/transact (connect) [{:db/id (d/tempid :db.part/user) :user/code (:code user)}]))
+(defn add-user [{:keys [code]}]
+  (d/transact (connect) [{:db/id (d/tempid :db.part/user) :user/code code}]))
 
 (defn get-user [user]
   (strip-namespaces (read-entity (user-id user))))
@@ -56,18 +56,16 @@
 (defn get-note [id]
   (strip-namespaces (read-entity id)))
 
-(defn add-board [board]
+(defn add-board [{:keys [user name]}]
   (let [temp-id (d/tempid :db.part/user)
-        board-name (:name board)
-        user-id (user-id {:code (:user board)})]
-    (d/transact (connect) [{:db/id temp-id :board/name board-name}
+        user-id (user-id {:code user})]
+    (d/transact (connect) [{:db/id temp-id :board/name name}
                            {:db/id user-id :user/boards temp-id}])))
 
-(defn add-note [note]
+(defn add-note [{:keys [user board text]}]
   (let [note-id (d/tempid :db.part/user)
-        note-text (:text note)
-        board-id (board-id {:user (:user note) :name (:board note)})]
-    (d/resolve-tempid (read-db) (:tempids @(d/transact (connect) [{:db/id note-id :note/text note-text}
+        board-id (board-id {:user user :name board})]
+    (d/resolve-tempid (read-db) (:tempids @(d/transact (connect) [{:db/id note-id :note/text text}
                                                                   {:db/id board-id :board/notes note-id}])) note-id)))
 (defn add-note-item [note-id text]
   (let [note (d/entity (read-db) note-id)]

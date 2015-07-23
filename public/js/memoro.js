c11b394b53818865d5672613037e8fef31974b6f
Aleksandar Terentic <aterentic@gmail.com>
2015-07-23 07:39:54 +0200

    Added query string parser.

--- public/js/memoro.js
+++ public/js/memoro.js
@@ -1,4 +1,17 @@
-$(function() {
+location.queryParams = {};
+
+(function(queryString) {
+    if (queryString === "") return;
+    if (queryString.substr(1) === "") return;
+    queryString.substr(1).split("&").forEach(function (pair) {
+	if (pair === "") return;
+	var parts = pair.split("=");
+	location.queryParams[parts[0]] = parts[1] &&
+            decodeURIComponent(parts[1].replace(/\+/g, " "));
+    });
+})(location.search);
+
+$(function() {  
     $("#create").click(() => { 
 	$.post( "/notes", function( data ) {
 	    window.location.href = "/note.html?note=" + data.identificator;

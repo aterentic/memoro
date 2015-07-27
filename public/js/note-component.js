3649e751dab4ce663141d0d83d203fa6e7c5203e
Aleksandar Terentic <aterentic@gmail.com>
2015-07-27 21:48:40 +0200

    Refactoring of note component.

--- public/js/note-component.js
+++ public/js/note-component.js
@@ -1,8 +1,18 @@
 var NoteItem  = React.createClass({
     render: function() {
+	function nop() {
+	    return false;
+	}
+	
+	function render_checkbox(props) {
+	    return (
+		<input type="checkbox" checked={props.checked==="true"} onChange={nop} />
+	    );
+	}
+	
 	return (
 	    <li className="note-item">
-		<input type="checkbox" checked={this.props.checked==="true"} onChange={function() { return false; }} />
+		{render_checkbox(this.props)}
 	        {this.props.text}
 	    </li>
 	);
@@ -20,14 +30,14 @@ var Note = React.createClass({
 	}.bind(this));
     },
     render: function() {
-	var items = this.state.data.items.map(function(item) {
+	function render_item(item) {
 	    return (
-		    <NoteItem text={item.text} checked={item["checked?"]} key={item.id}/>
+		    <NoteItem text={item["text"]} checked={item["checked?"]} key={item["id"]}/>
 	    );
-	});
+	}
 	return (
 	    <ul className="note">
-		{items}
+		{this.state.data.items.map(render_item)}
 	    </ul>
 	);
     }

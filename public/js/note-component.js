9a14a2e1e37d6f5821a3ba06ecf9d3e4c44b365f
Aleksandar Terentic <aterentic@gmail.com>
2015-07-29 14:08:27 +0200

    Added note items adding.

--- public/js/note-component.js
+++ public/js/note-component.js
@@ -19,14 +19,63 @@ var NoteItem  = React.createClass({
     }
 });
 
+var NoteItemForm = React.createClass({
+    handleKeyup: function(e) {
+	e.preventDefault();
+	if (e.keyCode == 13) {
+            React.findDOMNode(this.refs.form).submit();
+	}
+    },
+    handleSubmit: function(e) {
+	e.preventDefault();
+	var text = React.findDOMNode(this.refs.item).value.trim();
+	if (!text) {
+	    return;
+	}
+	this.props.onItemSubmit({text: text});
+	React.findDOMNode(this.refs.item).value = '';
+    },
+    render: function() {
+	return (
+		<form className="item form" ref="form" onSubmit={this.handleSubmit}>
+		    <input type="text" placeholder="New note item..." ref="item" onKeyup={this.handleKeyup} />
+		</form>
+	);
+  }
+});
+
 var Note = React.createClass({
+    noteUrl: function() {
+	return "notes/" + location.queryParams["note"];
+    },
+    itemsUrl: function() {
+	return "notes/" + location.queryParams["note"] + "/items";
+    },
+    handleItemSubmit: function(data) {
+	// TODO For some reason, done is not called.
+	$.ajax({
+	    url:this.itemsUrl(),
+	    type: "POST",
+	    data: JSON.stringify({
+		"text" : data.text,
+		"note" : new Number(this.state.data.id),
+		"priority": 1,
+		"checked?": false
+	    }),
+	    contentType:"application/json; charset=utf-8",
+	    dataType:"json"
+	}).always(function(e) {
+	    $.get(this.noteUrl()).done(function(data) {
+		this.setState({data: data});
+	    }.bind(this));	    
+	}.bind(this));
+    },
     getInitialState: function() {
 	return {data: {items: []}};
     },
     componentDidMount: function () {
-	var url = "notes/" + location.queryParams["note"] + "/items";
-	$.get(url).done(function(data) {
-	    this.setState({data: {items: data}});
+	$.get(this.noteUrl()).done(function(data) {
+	    this.setState({data: data});
 	}.bind(this));
     },
     render: function() {
@@ -38,6 +87,7 @@ var Note = React.createClass({
 	return (
 	    <ul className="note">
 		{this.state.data.items.map(render_item)}
+	        <NoteItemForm onItemSubmit={this.handleItemSubmit} />
 	    </ul>
 	);
     }
